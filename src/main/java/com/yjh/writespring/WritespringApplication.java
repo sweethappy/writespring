b515a6fd03d4ea4db02149e82fa282e7f1a31a25
youjh <1285990097@qq.com>
2022-04-11 15:32:58 +0800

    Initial commit

---
+++ src/main/java/com/yjh/writespring/WritespringApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.writespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WritespringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjh/writespring/WritespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.writespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
